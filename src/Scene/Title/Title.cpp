32f5009e637dd2695ea81e6d16b05cfab33da0fd
namaeanyting <2309427@s.asojuku.ac.jp>
2024-04-15 10:07:04 +0900

    title

--- src/Scene/Title/Title.cpp
+++ src/Scene/Title/Title.cpp
@@ -98,7 +98,10 @@ void Title::Step()
 		//}
 
 		gearAngle+=0.1;
-
+		lineX += 0.1;
+		lineY += 0.1;
+		lineW += 0.1;
+		lineH += 0.1;
 
 		//スペースキーで画面変化
 		if (Input::Key::Push(KEY_INPUT_SPACE))
@@ -154,25 +157,26 @@ void Title::Step()
 }
 
 //描画
-void Title::Draw()
-{
-	DrawGraph(0, 0, imageHandle[TITLE_TITLE], true);	//タイトル下地
-	DrawRotaGraph(-122, -106,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR1], true);
+void Title::Draw()//800 600
+{	
+	//タイトル下地
+	DrawGraph(0, 0, imageHandle[TITLE_TITLE], true);	
+
+	//左上
+	DrawRotaGraph(15, 0,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR1], true);
+
+	//左下
 	DrawRotaGraph(-42, 410,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR2], true);	
-	DrawRotaGraph(578, 673,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR2], true);	
-	DrawRotaGraph(-42, 410,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR3], true);	
 	DrawRotaGraph(34, 512,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR3], true);	
 	DrawRotaGraph(92, 429,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR3], true);	
 
-	DrawRotaGraph(578, 672,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR3], true);	
-	DrawRotaGraph(733, 556,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR3], true);	
-	
-	////雲
-	//for (int i = 0; i < 4; i++)
-	//{
-	//	DrawGraphF(cloudX[i][0], cloudY, imageHandle[TITLE_CLOUD1 + i], true);
-	//	DrawGraphF(cloudX[i][1], cloudY, imageHandle[TITLE_CLOUD1 + i], true);
-	//}
+	//右下
+	DrawRotaGraph(760, 460,1.0f, (int)gearAngle*-1, imageHandle[TITLE_GEAR3], true);	
+	DrawRotaGraph(663, 576,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR3], true);	
+
+	//タイトル下の線 座標、左上座標、描画するサイズ、ハンドル、透明化、画像反転有無  421 43
+	//DrawRectGraph(210, 344, lineX, lineY, lineW,lineH,imageHandle[TITLE_LINE], false,false);
+	DrawRectGraph(210, 344, 421, 43, 421,43,imageHandle[TITLE_LINE], true,false);
 
 //	//fadeで透明度変更
 //	SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade[0]);
