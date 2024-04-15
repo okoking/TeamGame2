7eb8f71c75a2d3152f69754eef205d1a26555da8
namaeanyting <2309427@s.asojuku.ac.jp>
2024-04-16 00:19:35 +0900

    enemy

--- src/Scene/Play/Play.cpp
+++ src/Scene/Play/Play.cpp
@@ -3,7 +3,7 @@
 
 //画像パス
 const char BACKGROUND_PATH[] = { "data/play/backGround.png" };		//プレイシーンの背景画像のパス
-const char ENEMY_PATH[] = { "data/play/Move1.png" };				//プレイシーンの背景画像のパス
+const char ENEMY_PATH[] = { "data/play/move1.png" };				//プレイシーンの背景画像のパス
 
 //プレイシーンの初期化
 void Play::Init()
@@ -52,8 +52,9 @@ void Play::Step()
 //プレイシーンの描画処理
 void Play::Draw()
 {
-	//背景描画
-	DrawGraph(500, 0, enemyhandle, true);
+	//敵500 0
+	DrawGraph(500, 0,enemyhandle, true);
+	//DrawRotaGraph(400, 300,1.0f,0.0f, enemyhandle, true);
 
 	DrawLineBox(HEART_INIT_POS_X - HEART_INIT_POS_Y, 0, HEART_INIT_POS_X + HEART_INIT_POS_Y, HEART_INIT_POS_Y + HEART_INIT_POS_Y,
 		GetColor(255, 255, 255));
@@ -64,10 +65,10 @@ void Play::Draw()
 	//文字の大きさを変更
 	SetFontSize(20);
 	//操作説明を書く
-	DrawFormatString(20, 465, GetColor(255, 255, 255), "来る方向に盾を向けろ");
-	DrawFormatString(20, 485, GetColor(255, 255, 255), "赤い矢は防ぐな");
-	DrawFormatString(20, 505, GetColor(255, 255, 255), "後は雰囲気でなんとかしろ");
-	DrawFormatString(20, 545, GetColor(255, 255, 255), "ガード成功数:%d",player.GetGuradCnt());
+	DrawFormatString(20, 100, GetColor(255, 255, 255), "来る方向に盾を向けろ");
+	DrawFormatString(20, 120, GetColor(255, 255, 255), "赤い矢は防ぐな");
+	DrawFormatString(20, 140, GetColor(255, 255, 255), "後は雰囲気でなんとかしろ");
+	DrawFormatString(500, 40, GetColor(255, 255, 255), "ガード成功数:%d",player.GetGuradCnt());
 	
 	DrawRotaGraph(400, 300, 1.0f, 0.0f, img_sikaku, true);
 	
